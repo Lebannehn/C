fc06b5dd2fee22ab94028912821a23ea5e3bd040
Karnimirie Lebannehn <Karnimirie Lebannehn>
2016-08-29 15:57:18 +0300

    if else

---
+++ if_else.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int a = 100;
+
+  if (a <=20) {
+    printf("This one is less than 20. a = %d\n",a);
+  } else {
+    printf("This one is more than 20. a= %d\n",a);
+  }
+  return 0;
+}
