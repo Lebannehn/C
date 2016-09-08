550ef571f94dac3d56f595086b0ca989e47f99b2
Lebannehn <lebannehn.karnimirie@gmail.com>
2016-09-08 13:44:55 +0300

    Update temperature_while.c

--- temperature_while.c
+++ temperature_while.c
@@ -14,7 +14,7 @@ int main () {
   fahr = lower;
   while (fahr <= upper) {
     celsius = 5 * (fahr-32)/9;
-    printf("%d\t%d\n",fahr,celsius);
+    printf("%3d\t%6d\n",fahr,celsius); /* with formatting %3d and %6d*/
     fahr = fahr + step;
   }
   return 0;
